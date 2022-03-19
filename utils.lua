ade1469242df97c766fb75a0aebb8dd1bc22af84
Baidicoot <aidanprattewart@gmail.com>
2022-03-19 12:06:50 +0000

    fall arrest

--- utils.lua
+++ utils.lua
@@ -2,6 +2,10 @@ local modules = peripheral.wrap "back"
 local PLAYER = "baidicoot"
 local pressedKeys = {}
 
+local LASE_KEY = keys.x
+local FLY_KEY = keys.r
+local FALL_KEY = keys.f
+
 function listenRoutine()
     while true do
         local ev, arg = os.pullEvent()
@@ -16,18 +20,12 @@ end
 -- drill
 
 function drillRoutine(power)
-    local run = false
     parallel.waitForAny(function()
     while true do
         local meta = modules.getMetaByName(PLAYER)
         
-        if run then modules.fire(meta.yaw, meta.pitch, power) end
-    end end, function()
-        while true do
-            local _, c = os.pullEvent "char"
-            if c == "x" then run = not run end
-        end
-    end)
+        if pressedKeys[LASE_KEY] then modules.fire(meta.yaw, meta.pitch, power) end
+    end end)
 end
 
 -- flight
@@ -37,7 +35,7 @@ function flightRoutine(power)
     while true do
         local meta = modules.getMetaByName(PLAYER)
         
-        if pressedKeys[keys.r] then modules.launch(meta.yaw, meta.pitch, power) end
+        if pressedKeys[FLY_KEY] then modules.launch(meta.yaw, meta.pitch, power) end
     end end)
 end
 
@@ -48,8 +46,8 @@ function fallArrestRoutine()
     while true do
         local meta = modules.getMetaByName(PLAYER)
 
-        if not pressedKeys[keys.r] and meta.motionY >= 5 then
-            modules.launch(0, 270, 2)
+        if not (pressedKeys[FLY_KEY] or pressedKeys[FALL_KEY]) and meta.motionY <= -0.2 then
+            modules.launch(0, 270, 0.3)
         end
     end end)
 end
