9e183551bb0540493939815d6c471908fef0b75e
Baidicoot <aidanprattewart@gmail.com>
2022-04-10 14:37:28 +0100

    test2

--- autominer.lua
+++ autominer.lua
@@ -1,21 +1,21 @@
 local laser = peripheral.find "plethora:laser"
 
+local dir = tonumber(args[1])
+
 local function main()
     while true do
         if turtle.detect() then
-            laser.fire(90, 0, 5)
+            laser.fire(dir*90, 0, 5)
         end
         local ok, reason = turtle.forward()
-        if ok then
-            break
-        elseif reason == "Out of fuel" then
+        if not ok and reason == "Out of fuel" then
             print("Refuel")
             turtle.refuel()
-            sleep(1)
         end
         laser.fire(0, 270, 5)
         laser.fire(0, 270, 5)
     end
+    os.sleep(0)
 end
 
 main()
